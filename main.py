35b82755a0c0d8bcc9c403e50bc8223113e0fa02
Andras Gyarmati <bandieee16@gmail.com>
2023-11-04 22:29:55 +0100

    add display method

--- main.py
+++ main.py
@@ -11,75 +11,108 @@ class TriangularLattice:
 
     def add_tile(self, coordinates):
         if coordinates not in self.tiles:
-            tile = HexagonalTile()
-            self.tiles[coordinates] = tile
+            self.tiles[coordinates] = HexagonalTile()
+            self.tiles[coordinates].occupied = True
 
     def remove_tile(self, coordinates):
-        if coordinates in self.tiles:
+        if coordinates in self.tiles and not self.tiles[coordinates].occupied:
             del self.tiles[coordinates]
 
-    def move_robot(self, new_position):
-        if new_position in self.tiles or new_position == self.robot_position:
+    def move_robot(self, direction):
+        new_position = self.get_neighbor(self.robot_position, direction)
+        if new_position and (new_position not in self.tiles or not self.tiles[new_position].occupied):
             self.robot_position = new_position
 
     def pick_up_tile(self):
-        if self.robot_position in self.tiles and not self.robot_has_tile:
+        if self.robot_position in self.tiles and self.tiles[self.robot_position].occupied and not self.robot_has_tile:
             self.tiles[self.robot_position].occupied = False
             self.robot_has_tile = True
 
     def place_tile(self):
         if self.robot_has_tile:
-            self.tiles[self.robot_position].occupied = True
+            if self.robot_position not in self.tiles:
+                self.add_tile(self.robot_position)
+            else:
+                self.tiles[self.robot_position].occupied = True
             self.robot_has_tile = False
-
-    def move_robot_with_tile(self, direction):
-        if self.robot_has_tile:
-            new_position = self.get_neighbor(self.robot_position, direction)
-            if new_position:
-                if new_position in self.tiles:
-                    self.tiles[new_position].occupied = True
-                self.tiles[self.robot_position].occupied = False
-                self.robot_position = new_position
 
     def get_neighbor(self, position, direction):
         x, y = position
         if direction == 'N':
-            return (x, y - 1)
-        elif direction == 'NE':
-            return (x + 1, y - 1)
-        elif direction == 'SE':
-            return (x + 1, y)
-        elif direction == 'S':
             return (x, y + 1)
+        elif direction == 'NE':
+            return (x + 1, y if x % 2 == 0 else y + 1)
+        elif direction == 'SE':
+            return (x + 1, y if x % 2 != 0 else y - 1)
+        elif direction == 'S':
+            return (x, y - 1)
         elif direction == 'SW':
-            return (x - 1, y + 1)
+            return (x - 1, y if x % 2 != 0 else y - 1)
         elif direction == 'NW':
-            return (x - 1, y)
+            return (x - 1, y if x % 2 == 0 else y + 1)
         return None
 
     def is_connected(self):
+        if not self.tiles:
+            return False
         visited = set()
-        stack = [self.robot_position]
+        stack = [next(iter(self.tiles))]  # Start from any tile
         while stack:
             current_position = stack.pop()
             if current_position not in visited:
                 visited.add(current_position)
-                # check for the six neighbors
                 for direction in ['N', 'NE', 'SE', 'S', 'SW', 'NW']:
                     neighbor = self.get_neighbor(current_position, direction)
-                    if neighbor and neighbor in self.tiles and self.tiles[neighbor].occupied:
+                    if neighbor in self.tiles and self.tiles[neighbor].occupied:
                         stack.append(neighbor)
-        return len(visited) == len(self.tiles) + 1
+        return len(visited) == len([t for t in self.tiles.values() if t.occupied])
+
+    def display(self):
+        """Prints a simple representation of the grid with tiles and the robot's position."""
+        # Determine grid bounds
+        min_x = min(self.tiles.keys(), key=lambda t: t[0])[0]
+        max_x = max(self.tiles.keys(), key=lambda t: t[0])[0]
+        min_y = min(self.tiles.keys(), key=lambda t: t[1])[1]
+        max_y = max(self.tiles.keys(), key=lambda t: t[1])[1]
+
+        # Display the grid
+        for y in range(max_y, min_y - 1, -1):
+            line = ""
+            for x in range(min_x, max_x + 1):
+                if (x, y) in self.tiles and self.tiles[(x, y)].occupied:
+                    line += "[X]" if (x, y) != self.robot_position else "[R]"
+                elif (x, y) == self.robot_position:
+                    line += "[r]"
+                else:
+                    line += " . "
+            print(line)
 
 
-lattice = TriangularLattice()
-lattice.add_tile((0, 0))
-lattice.add_tile((1, 0))
-lattice.add_tile((0, 1))
-lattice.add_tile((1, 1))
+def main():
+    lattice = TriangularLattice()
+    # Add tiles to the lattice
+    lattice.add_tile((0, 0))
+    lattice.add_tile((1, 0))
+    lattice.add_tile((0, 1))
+    lattice.add_tile((1, 1))
 
-lattice.move_robot((0, 0))
-lattice.pick_up_tile()
-lattice.move_robot((1, 0))
-lattice.place_tile()
-lattice.move_robot_with_tile("N")
+    # Initial robot position and tile picking
+    lattice.move_robot('N')
+    lattice.pick_up_tile()
+    lattice.display()
+
+    # Robot movement with a tile
+    lattice.move_robot('S')
+    lattice.place_tile()
+    lattice.display()
+
+    # Robot movement with a tile to a new position
+    lattice.pick_up_tile()
+    lattice.move_robot('NE')
+    lattice.place_tile()
+    lattice.display()
+
+
+# Run the main function
+if __name__ == "__main__":
+    main()
